88ab74efb9a401737f74a9a6c6752ed59cfcb7bb
Tim Hütz <tim@huetz.biz>
2013-04-15 16:57:51 +0200

    Changed the default editor to vim

--- rc.lua
+++ rc.lua
@@ -39,7 +39,7 @@ beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
-editor = os.getenv("EDITOR") or "editor"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
