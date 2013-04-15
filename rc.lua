52cddef17d66f8b17a10365814429f9571536153
Tim Hütz <tim@huetz.biz>
2013-04-15 16:59:52 +0200

    Modified some of the menus

--- rc.lua
+++ rc.lua
@@ -81,15 +81,15 @@ end
 -- {{{ Menu
 -- Create a laucher widget and a main menu
 myawesomemenu = {
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", awesome.quit }
+--   { "manual", terminal .. " -e man awesome" },
+--   { "edit config", editor_cmd .. " " .. awesome.conffile },
+   { "restart wm", awesome.restart },
+   { "logout", awesome.quit }
 }
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "Debian", debian.menu.Debian_menu.Debian },
-                                    { "open terminal", terminal }
+                                    { "open terminal", terminal },
+                                    { "debian", debian.menu.Debian_menu.Debian }
                                   }
                         })
 
