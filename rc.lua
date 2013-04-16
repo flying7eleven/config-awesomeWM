46e8cb72be9420c5857b0b484b2bbc51c49021c9
Tim Hütz <tim@huetz.biz>
2013-04-16 10:18:24 +0200

    Re-enabled the app launch again
    The apps are launched during loging again. They are not started in another instance,
    if the app is already running.

--- rc.lua
+++ rc.lua
@@ -49,6 +49,21 @@ function myrestart()
 	awesome.restart()
 end
 
+--
+function run_once(prg,arg_string,pname,screen)
+	if not prg then
+		do return nil end
+	end
+	if not pname then
+		pname = prg
+	end
+	if not arg_string then
+		awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. "' || (" .. prg .. ")",screen)
+	else
+		awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. " ".. arg_string .."' || (" .. prg .. " " .. arg_string .. ")",screen)
+	end
+end
+
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
 -- If you do not like this or do not have such a key,
@@ -390,9 +405,9 @@ client.add_signal("unfocus", function(c) c.border_color = beautiful.border_norma
 -- {{{ Startup applications
 -- A list of commands executed after the login process
 -- awful.util.spawn( "conky -c ~/.config/awesome/configs/conky.conf" );
--- awful.util.spawn( "google-chrome" );
--- awful.util.spawn( "thunderbird" );
--- awful.util.spawn( "skype" );
--- awful.util.spawn( "pidgin" );
--- awful.util.spawn( "gnome-terminal" );
+run_once( "google-chrome", nil, "/opt/google/chrome/chrome" );
+run_once( "thunderbird", nil, "/usr/lib/thunderbird/thunderbird" );
+run_once( "skype" );
+run_once( "pidgin" );
+run_once( "gnome-terminal" );
 -- }}}
