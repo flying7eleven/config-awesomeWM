cc3de2e3a3e245f14178c7bc13f7e32e3966b886
Tim Hütz <tim@huetz.biz>
2013-04-20 18:01:31 +0200

    The tags will now be set based on the current hostname

--- inc/tags.lua
+++ inc/tags.lua
@@ -1,6 +1,9 @@
 -- vi: set tw=1000 ts=4 autoindent smartindent cindent:
 -- Some ideas copied from https://github.com/JoeD84/awesome
 -- {{{ Tags
+local hostname = io.popen( "uname -n" ):read()
+
+-- Prepare the tags for the different scenarios
 tags = {}
 tags.workSettings = {
 	{ name = "1:www",		layout = awful.layout.suit.max,			wmfact = 1.0 },
@@ -14,11 +17,29 @@ tags.workSettings = {
 	{ name = "9:NA",		layout = awful.layout.suit.tile.left,	wmfact = 0.5 },
 }
 
+tags.privateSettings = {
+	{ name = "1:www",		layout = awful.layout.suit.max,			wmfact = 1.0 },
+	{ name = "2:email",		layout = awful.layout.suit.max,			wmfact = 1.0 },
+	{ name = "3:terminal",	layout = awful.layout.suit.tile.left,	wmfact = 0.5 },
+	{ name = "4:steam",		layout = awful.layout.suit.max,			wmfact = 1.0 },
+	{ name = "5:NA",		layout = awful.layout.suit.tile.left,	wmfact = 0.5 },
+	{ name = "6:NA",		layout = awful.layout.suit.tile.left,	wmfact = 0.5 },
+	{ name = "7:NA",		layout = awful.layout.suit.tile.left,	wmfact = 0.5 },
+	{ name = "8:NA",		layout = awful.layout.suit.tile.left,	wmfact = 0.5 },
+	{ name = "9:NA",		layout = awful.layout.suit.tile.left,	wmfact = 0.5 },
+}
+
 -- based on the host name, we select the configuration we want to use
--- TODO: insert the code lines which check the host name
+if hostname == "zockepeter" then
+	tags.usedTags = tags.privateSettings
+else
+	tags.usedTags = tags.workSettings
+end
+
+-- set the actually used tags
 for s = 1, 1 do
 	tags[ s ] = {}
-	for i, v in ipairs( tags.workSettings ) do
+	for i, v in ipairs( tags.usedTags ) do
 		tags[ s ][ i ] = tag( { name = v.name } )
 		tags[ s ][ i ].screen = s
 		awful.tag.setproperty( tags[ s ][ i ], "layout", v.layout )
