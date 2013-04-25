7d7bf9b836a7b021254e0c6ca249989b5a0c0efe
Tim Hütz <tim@huetz.biz>
2013-04-25 13:25:05 +0200

    The script will now work in awesome 3.5

--- inc/wallpaper.lua
+++ inc/wallpaper.lua
@@ -4,21 +4,26 @@
 -- configuration - edit to your liking
 wp_index = 1
 wp_timeout  = 3600 -- change the background image every hour
-wp_path = "~/.config/awesome/themes/work/wallpapers/"
+wp_user = io.popen( "whoami" ):read()
+wp_path = "/home/" .. wp_user .. "/.config/awesome/themes/work/wallpapers/"
 wp_files = { "01.jpg", "02.jpg" }
 
 -- set the initial background
 wp_init_timer = timer { timeout = 5 }
-wp_init_timer:add_signal( "timeout", function()
+wp_init_timer:connect_signal( "timeout", function()
 	wp_init_timer:stop()
-	awful.util.spawn_with_shell( "awsetbg -f " .. wp_path .. wp_files[ wp_index ] )
+	for s = 1, screen.count() do
+		gears.wallpaper.maximized( wp_path .. wp_files[ wp_index ], s, true )
+	end
 end)
 
 -- setup the timer for changing the background images
 wp_timer = timer { timeout = wp_timeout }
-wp_timer:add_signal( "timeout", function()
+wp_timer:connect_signal( "timeout", function()
 	-- set wallpaper to current index
-	awful.util.spawn_with_shell( "awsetbg -f " .. wp_path .. wp_files[ wp_index ] )
+	for s = 1, screen.count() do
+		gears.wallpaper.maximized( wp_path .. wp_files[ wp_index ], s, true )
+	end
 
 	-- stop the timer (we don't need multiple instances running at the same time)
 	wp_timer:stop()
