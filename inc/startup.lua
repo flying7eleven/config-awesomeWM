85a152f784a658271c2e799d75e4a960dc25073e
Tim Hütz <tim@huetz.biz>
2013-04-25 19:39:31 +0200

    Prepared some autostart commands

--- inc/startup.lua
+++ inc/startup.lua
@@ -1,9 +1,8 @@
 -- vi: set tw=1000 ts=4 autoindent smartindent cindent:
 -- TODO: idea from ...
 -- {{{ Startup applications
-local hostname = io.popen( "uname -n" ):read()
 
---
+-- function for ensuring that every program just runs once
 function run_once(prg,arg_string,pname,screen)
 	if not prg then
 		do return nil end
@@ -20,20 +19,14 @@ end
 
 -- start the applications which should always start
 -- awful.util.spawn( "conky -c ~/.config/awesome/configs/conky.conf" );
-run_once( "google-chrome", nil, "/opt/google/chrome/chrome" );
-run_once( "thunderbird", nil, "/usr/lib/thunderbird/thunderbird" );
-
--- based on the host name, start the desired applications
-if hostname == "zockepeter" then
-	run_once( "gnome-terminal" );
-	run_once( "steam" );
-else
-	run_once( "google-chrome", "-app=https://app.asana.com", "/opt/google/chrome/chrome" );
-	run_once( "google-chrome", "-app=https://www.newsblur.com", "/opt/google/chrome/chrome" );
-	run_once( "google-chrome", "-app=https://calendar.google.com", "/opt/google/chrome/chrome" );
-	run_once( "eclipse" );
-	run_once( "skype" );
-	run_once( "pidgin" );
-	run_once( "gnome-terminal" );
-end
+--run_once( "chromium", nil, "/usr/lib/chromium/chromium" );
+--run_once( "chromium", "--app=https://calendar.google.com", "/usr/lib/chromium/chromium" );
+--run_once( "chromium", "--app=https://app.asana.com", "/usr/lib/chromium/chromium" );
+--run_once( "chromium", "--app=https://www.newsblur.com", "/usr/lib/chromium/chromium" );
+--run_once( "thunderbird" );
+--run_once( "terminator", nil, "/usr/bin/python2 /usr/bin/terminator" );
+--run_once( "skype", nil, "/usr/lib32/skype/skype" );
+--run_once( "pidgin" );
+--run_once( "eclipse", nil, "/bin/bash /usr/bin/eclipse" );
+--run_once( "steam", nil, "bash /usr/bin/steam" );
 -- }}}
