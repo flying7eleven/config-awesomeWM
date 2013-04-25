7ec16e4f2f8b94803a4ed4f757f38a5cbeb8d45c
Tim Hütz <tim@huetz.biz>
2013-04-25 20:05:53 +0200

    Removed a separate rule for the private computer :)

--- inc/rules/terminal.lua
+++ inc/rules/terminal.lua
@@ -1,19 +1,7 @@
 -- vi: set tw=1000 ts=4 autoindent smartindent cindent:
 -- Some ideas copied from https://github.com/JoeD84/awesome
-rule_terminal_01 = {}
-
-rule_terminal_01.private = {
+rule_terminal_01 = {
 	rule = { class = "Terminator" },
-	properties = { tag = tags[ 1 ][ 3 ] }
+	properties = { tag = tags[ 1 ][ 6 ] },
+	opacity = 0.8
 }
-
-rule_terminal_01.work = {
-	rule = { class = "Terminator" },
-	properties = { tag = tags[ 1 ][ 6 ] }
-}
-
-if wmHostname == "zockepeter" then
-	rule_terminal_01.currentlyActive = rule_terminal_01.private
-else
-	rule_terminal_01.currentlyActive = rule_terminal_01.work
-end
