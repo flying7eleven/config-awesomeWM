27314f1a2c5de8b02127c46e0df3c6e2ae2779a3
Tim Hütz <tim@huetz.biz>
2013-04-26 14:09:36 +0200

    Enabled the use of the old (default) theme

--- rc.lua
+++ rc.lua
@@ -39,6 +39,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+--beautiful.init("/home/thuetz/.config/awesome/themes/anon/theme.lua" )
 
 -- This is used later as the default terminal and editor to run.
 terminal = "terminator"
