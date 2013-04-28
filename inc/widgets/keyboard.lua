6770fa2a2085ca9865a792ae61548800b6eb28b2
Tim Hütz <tim@huetz.biz>
2013-04-28 10:24:06 +0200

    Fixed the non-working keyboard mapping switcher

--- inc/widgets/keyboard.lua
+++ inc/widgets/keyboard.lua
@@ -5,15 +5,15 @@ kbdcfg.cmd = "setxkbmap"
 kbdcfg.layout = { "us", "de" }
 kbdcfg.current = 1  -- us is our default layout
 kbdcfg.widget = wibox.widget.textbox()
-kbdcfg.widget.text = " " .. kbdcfg.layout[kbdcfg.current] .. " "
+kbdcfg.widget:set_text( " " .. kbdcfg.layout[ kbdcfg.current ] .. " " )
 kbdcfg.switch = function ()
-	kbdcfg.current = kbdcfg.current % #(kbdcfg.layout) + 1
-	local t = " " .. kbdcfg.layout[kbdcfg.current] .. " "
-	kbdcfg.widget.text = t
-	os.execute( kbdcfg.cmd .. t )
+	kbdcfg.current = kbdcfg.current % #( kbdcfg.layout ) + 1
+	local t = kbdcfg.layout[ kbdcfg.current ]
+	kbdcfg.widget:set_text( " " .. t .. " " )
+	os.execute( kbdcfg.cmd .. " " .. t )
 end
 
 -- Mouse bindings for clicking this widget
-kbdcfg.widget:buttons(awful.util.table.join(
-	awful.button({ }, 1, function () kbdcfg.switch() end)
-))
+kbdcfg.widget:buttons(
+	awful.util.table.join( awful.button( {}, 1, function () kbdcfg.switch() end ) )
+)
