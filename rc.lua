764118d298a07632e6c95f582a5ef191cd48b4fe
Tim Hütz <tim@huetz.biz>
2013-06-10 08:44:55 +0200

    Renamed some menu entries

--- rc.lua
+++ rc.lua
@@ -86,10 +86,8 @@ end
 -- {{{ Menu
 -- Create a laucher widget and a main menu
 myawesomemenu = {
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", awesome.quit }
+   { "reload config", awesome.restart },
+   { "exit awesome", awesome.quit }
 }
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
