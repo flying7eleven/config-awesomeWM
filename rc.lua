3c656532bad0dd7747f9e7312dabac0cfe908d78
Tim Hütz <tim@huetz.biz>
2013-06-13 22:43:03 +0200

    Added a command to ensure that the networkmanager gets started

--- rc.lua
+++ rc.lua
@@ -428,3 +428,7 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- {{{ Start some stuff
+awful.util.spawn_with_shell( "pgrep -u $USER -x nm-applet > /dev/null || (nm-applet &)" )
+-- }}}
