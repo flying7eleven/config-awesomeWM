d82787fece8f0f3e3ae5d7cc50e0d5ba3197b6ae
Tim Hütz <tim@huetz.biz>
2013-08-03 11:24:47 +0200

    Added a rule for Mumble

--- rc.lua
+++ rc.lua
@@ -352,6 +352,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "Steam" },
       properties = { floating = true } },
+    { rule = { class = "Mumble" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
