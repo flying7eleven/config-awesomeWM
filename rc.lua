4ef2080ae2db56aab7ca172d013c8de635e152b6
Tim Hütz <tim@huetz.biz>
2014-01-17 07:19:49 +0100

    Added a rule for fullscreen flash videos to work

--- rc.lua
+++ rc.lua
@@ -373,6 +373,8 @@ awful.rules.rules = {
       properties = { floating = false } },
     { rule = { name = "RealLife Character Editor" },
       properties = { floating = true } },
+    { rule = { instance = "plugin-container" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
