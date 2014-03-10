046d5619147d49ecf875d95035f71bc21e537934
Tim Hütz <tim@huetz.biz>
2014-03-10 17:59:24 +0100

    Updated the tags for the personal computer

--- rc.lua
+++ rc.lua
@@ -85,7 +85,7 @@ if screen.count() > 1 then
 	tags[ 1 ] = awful.tag({ "1:term", "2:www", "3:netbeans", "4:spyder", 5, 6, 7, 8, 9 }, 1, layouts[2])
 	tags[ 2 ] = awful.tag({ "1:spotify", 2, 3, 4, 5, 6, 7, 8, 9 }, 2, layouts[2])
 else
-	tags[ 1 ] = awful.tag({ "1:term", "2:www", "3:email", "4:steam", "5:spotify", "6:ide", 7, 8, 9 }, 1, layouts[2])
+	tags[ 1 ] = awful.tag({ "1:term", "2:www", "3:steam", "4:spotify", "5:netbeans", 6, 7, 8, 9 }, 1, layouts[2])
 end
 -- }}}
 
