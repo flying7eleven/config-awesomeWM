9865a080e8a1840e64ec4053f53ce93766630e8e
Tim Hütz <tim@huetz.biz>
2015-02-05 09:24:14 +0100

    Removed the old rules files for the WM

--- inc/rules/terminal.lua
+++
@@ -1,7 +0,0 @@
--- vi: set tw=1000 ts=4 autoindent smartindent cindent:
--- Some ideas copied from https://github.com/JoeD84/awesome
-rule_terminal_01 = {
-	rule = { class = "Terminator" },
-	properties = { tag = tags[ 1 ][ 4 ] },
-	opacity = 0.8
-}
--- inc/rules/www.lua
+++
@@ -1,6 +0,0 @@
--- vi: set tw=1000 ts=4 autoindent smartindent cindent:
--- Some ideas copied from https://github.com/JoeD84/awesome
-rule_www_01 = {
-	rule = { class = "Chromium" },
-	properties = { tag = tags[ 1 ][ 1 ] }
-}
